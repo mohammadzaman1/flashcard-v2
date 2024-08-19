3dbde17bc105f2f4e22a2edbe9282c42f6d6d5fb
mohammadzaman1 <mdashikuzzaman17@gmail.com>
2024-08-19 19:02:21 -0400

    gtag added

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs"
-
+import Script from 'next/script'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,8 +13,22 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7QPHPP6Q3F"></Script>
+        <Script id="g-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-7QPHPP6Q3F');
+          `}
+        </Script>
+
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   )
 }
+
+
+
+
