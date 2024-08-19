982e64bc3f1853f03851ffbeabd4bffac837c0bd
mohammadzaman1 <mdashikuzzaman17@gmail.com>
2024-08-19 19:13:20 -0400

    gtag updated

--- app/layout.js
+++ app/layout.js
@@ -13,16 +13,17 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7QPHPP6Q3F"></Script>
-        <Script id="g-analytics">
-          {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        <head>
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7QPHPP6Q3F"></Script>
+          <Script id="g-analytics">
+            {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-            gtag('config', 'G-7QPHPP6Q3F');
-          `}
-        </Script>
-
+              gtag('config', 'G-7QPHPP6Q3F');
+            `}
+          </Script>
+        </head>
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
